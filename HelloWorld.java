c7d5ccc4572cf5d54492cfdde0babe941d480bec
426Manishkumar <manishkumar426001@gmail.com>
2026-02-20 12:19:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub + Jenkins! Webhook woeking!");
+        System.out.println("Hello from GitHub + Jenkins! Webhook working!");
     }
 }
 //webhook
